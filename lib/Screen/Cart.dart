e3141a5cd01b9dae576fdbfb92453904a4b7513d
Homen Nath <homennath7638@gmail.com>
2024-03-06 16:20:48 +0530

    Cart screen done

--- lib/Screen/Cart.dart
+++ lib/Screen/Cart.dart
@@ -3,27 +3,24 @@ import 'package:shoeshop/Screen/DetailsScreen.dart';
 import 'package:shoeshop/Screen/HomeScreen.dart';
 import 'package:shoeshop/Widgets/CustomCard.dart';
 
-class Cart extends StatelessWidget {
+class Cart extends StatefulWidget {
   final List<CartItem> cartItems;
   Cart({required this.cartItems});
 
   @override
+  _CartState createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: Colors.blue,
         leading: Padding(
           padding: EdgeInsets.only(left: 15.0),
           child: Container(
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.zero,
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    side: const BorderSide(color: Colors.black26),
-                  )),
-              onPressed: () {
+            child: InkWell(
+              onTap: () {
                 Navigator.pop(context);
               },
               child: Icon(
@@ -57,7 +54,7 @@ class Cart extends StatelessWidget {
                       padding: const EdgeInsets.all(4.0),
                       child: InkWell(
                         onTap: () {
-                          print(cartItems);
+                          print(widget.cartItems);
                         },
                         child: Image.asset('assets/images/bookCart.png'),
                       ),
@@ -66,7 +63,6 @@ class Cart extends StatelessWidget {
                 ),
                 Positioned(
                   top: 0,
-                  // left: 0.5,
                   child: Container(
                     width: 20,
                     height: 20,
@@ -75,13 +71,14 @@ class Cart extends StatelessWidget {
                       borderRadius: BorderRadius.circular(50),
                     ),
                     child: Center(
-                        child: Text(
-                      cartItems.length.toString(),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                      child: Text(
+                        widget.cartItems.length.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    )),
+                    ),
                   ),
                 ),
               ],
@@ -89,61 +86,258 @@ class Cart extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 15.0),
-        child: SizedBox(
-            width: MediaQuery.of(context).size.width * 1.0,
-            // height: MediaQuery.of(context).size.height * 1.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: cartItems.length,
-                    itemBuilder: (context, index) {
-                      final cartItem = cartItems[index];
-                      return Center(
+      body: Container(
+        // color: Colors.blue,
+        width: MediaQuery.of(context).size.width * 1.0,
+        height: MediaQuery.of(context).size.height * 1.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 1.0,
+                height: MediaQuery.of(context).size.height * 0.63,
+                // color: Colors.amber,
+                child: ListView.builder(
+                  itemCount: widget.cartItems.length,
+                  itemBuilder: (context, index) {
+                    final cartItem = widget.cartItems[index];
+                    return Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
                         child: Card(
                           elevation: 1,
                           child: SizedBox(
                             width: MediaQuery.of(context).size.width * 0.9,
-                            height: MediaQuery.of(context).size.height * 0.2,
+                            // height: MediaQuery.of(context).size.height * 0.2,
                             child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
-                                Column(
-                                  children: [
-                                    Text("abc"),
-                                    Text("abc"),
-                                    Text("abc"),
-                                    Row(
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.4,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
                                       children: [
-                                        Icon(Icons.remove),
-                                        Text("1"),
-                                        Icon(Icons.add),
+                                        Text(
+                                          cartItem.name,
+                                          style: const TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          cartItem.typeShoe,
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color:
+                                                Colors.black.withOpacity(0.3),
+                                          ),
+                                        ),
+                                        Text(
+                                          "\$${cartItem.price}",
+                                          style: const TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(top: 20.0),
+                                          child: Container(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.2,
+                                            decoration: BoxDecoration(
+                                              border: Border.all(
+                                                color: Colors.black
+                                                    .withOpacity(0.1),
+                                                width: 1,
+                                              ),
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                            ),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                InkWell(
+                                                  onTap: () {
+                                                    setState(() {
+                                                      if (cartItem.quantity !=
+                                                          1) {
+                                                        cartItem.quantity -= 1;
+                                                      }
+                                                    });
+                                                  },
+                                                  child: Icon(Icons.remove),
+                                                ),
+                                                Text(
+                                                  cartItem.quantity.toString(),
+                                                  style: const TextStyle(
+                                                    fontSize: 16,
+                                                    color: Color(0xFFFDAD00),
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                                InkWell(
+                                                  onTap: () {
+                                                    setState(() {
+                                                      if (cartItem.quantity !=
+                                                          10) {
+                                                        cartItem.quantity += 1;
+                                                      }
+                                                    });
+                                                  },
+                                                  child: Icon(Icons.add),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
                                       ],
                                     ),
-                                  ],
+                                  ),
                                 ),
-                                Stack(
-                                  alignment: Alignment.center,
-                                  children: [
-                                    Positioned(
-                                      child: Text("abc"),
-                                    ),
-                                  ],
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.4,
+                                  child: Stack(
+                                    alignment: Alignment.center,
+                                    children: [
+                                      Positioned(
+                                        child: Image.asset(cartItem.imgPath),
+                                      ),
+                                      Positioned(
+                                        top: 0,
+                                        right: 0,
+                                        child: Icon(
+                                          Icons.bookmark_outline_sharp,
+                                          color: Colors.black.withOpacity(0.3),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        bottom: 0,
+                                        right: 0,
+                                        child: Icon(
+                                          Icons.delete_outline,
+                                          color: Colors.black.withOpacity(0.3),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ],
                             ),
                           ),
                         ),
-                      );
-                    },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            applyCoupn(),
+            Pricing(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget applyCoupn() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: MediaQuery.of(context).size.height * 0.1,
+        // color: Colors.amber,
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: "Apply Coupan Code",
+            suffixIcon: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFFDAD00),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text("APPLY"),
                   ),
                 ),
               ],
-            )),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Colors.black.withOpacity(0.1),
+              ),
+            ),
+          ),
+        ),
       ),
-      // backgroundColor: Colors.amber,
+    );
+  }
+
+  Widget Pricing() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 1.0,
+      height: MediaQuery.of(context).size.height * 0.09,
+      // color: Colors.red,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Total Payable',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black.withOpacity(0.4),
+                    ),
+                  ),
+                  const Text(
+                    "\$1115.00",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )),
+          Padding(
+            padding: const EdgeInsets.only(right: 25.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFFFDAD00),
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {},
+              child: Text(
+                "CHECKOUT",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
